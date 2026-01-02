d7f7543d75accba2066950827c05fdfee4f106f6
minza <minza@DESKTOP-UUEE2ME>
2026-01-02 22:19:50 +0630

    Blog Controller CRUD

--- DotNetTrainingEFCoreAjax.MvcAjax/Controllers/BlogController.cs
+++ DotNetTrainingEFCoreAjax.MvcAjax/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using DotNetTrainingEFCoreAjax.Database.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNetTrainingEFCoreAjax.MvcAjax.Controllers
 {
@@ -17,5 +18,90 @@ namespace DotNetTrainingEFCoreAjax.MvcAjax.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var blogs = await _db.BlogTables
+                            .OrderByDescending(x => x.BlogId)
+                            .ToListAsync();
+            return Json(blogs);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Save(BlogTable blog)
+        {
+            await _db.BlogTables.AddAsync(blog);
+            var result = await _db.SaveChangesAsync();
+            var response = new
+            {
+                IsSuccess = result > 0,
+                Message = result > 0 ? "Saving Successful" : "Failed to save"
+            };
+            return Json(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id) 
+        {
+            var blog = await _db.BlogTables.AsNoTracking().FirstOrDefaultAsync(x => x.BlogId == id);
+            if (blog is null)
+            {
+                return Json(new { IsSuccess = false, Message = "Blog not found" });
+            }
+            return Json(blog);
+        }
+
+        public IActionResult EditBlog(int id)
+        {
+            ViewBag.BlogId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, BlogTable blog)
+        {
+            var item = await _db.BlogTables.FirstOrDefaultAsync(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Json(new { IsSuccess = false, Message = "Blog not found" });
+            }
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+
+            var result = await _db.SaveChangesAsync();
+            var response = new
+            {
+                IsSuccess = result > 0,
+                Message = result > 0 ? "Update Successful" : "Failed to update"
+            };
+            return Json(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _db.BlogTables.FirstOrDefaultAsync(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Json(new { IsSuccess = false, Message = "Blog not found" });
+            }
+            _db.BlogTables.Remove(item);
+            var result = await _db.SaveChangesAsync();
+            var response = new
+            {
+                IsSuccess = result > 0,
+                Message = result > 0 ? "Delete Successful" : "Failed to delete"
+            };
+            return Json(response);
+        }
+
+        
     }
 }
